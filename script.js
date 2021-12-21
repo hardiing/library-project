039e626bfc28f76113fa46dcd6664eb83341cba0
Nathan Harding <nathanharding02@gmail.com>
2021-12-20 23:52:25 -0700

    Added button functionality

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let myLibrary = [];
-const displayLibrary = document.querySelector(".library");
-const newBtn = document.querySelector(".newBook");
+const bookName = document.querySelector("#title");
+const bookAuthor = document.querySelector("#author");
+const pageCount = document.querySelector("#pages");
+const bookStatus = document.querySelector("#status");
+const newBtn = document.querySelector(".addBook");
 
 function Book(name, author, pages, read) {
     this.name = name;
@@ -12,40 +15,63 @@ function Book(name, author, pages, read) {
     }
 }
 
-function addBookToLibrary() {
-    let name = prompt("Enter book name");
-    let author = prompt("Enter author");
-    let pages = prompt("Enter number of pages");
-    let read = prompt("Have you read this book?");
-
-    let newBook = new Book(name, author, pages, read);
-    myLibrary.push(newBook);
-}
-
-function addBook(name, author, pages, read) {
-    let newBook = new Book(name, author, pages, read);
-    myLibrary.push(newBook);
-}
-
 let starsight = new Book("Starsight", "Sanderson", 457, "yes");
 myLibrary.push(starsight);
 let skyward = new Book("Skyward", "Sanderson", 503, "yes");
 myLibrary.push(skyward);
 let cytonic = new Book("Cytonic", "Sanderson", 500, "no");
 myLibrary.push(cytonic);
-console.log(myLibrary);
-displayBooks();
 
-function displayBooks() {
-    for (let i = 0; i <= myLibrary.length; i++) {
+/* function displayBooks() {
+    for (let i = 0; i < myLibrary.length; i++) {
         const newDiv = document.createElement("div");
         let bookInfo = myLibrary[i].card();
         newDiv.textContent = bookInfo;
         const currentDiv = document.getElementById("main");
         document.body.appendChild(newDiv);
     }
+} */
+
+function displayBooks() {
+    for (let i = 0; i < myLibrary.length; i++) {
+        const tableDiv = document.getElementById("table");
+        let bookInfo = myLibrary[i].card();
+        //newDiv.textContent = bookInfo;
+        const currentDiv = document.getElementById("main");
+        //document.body.appendChild(newDiv);
+    }
 }
 
-//addBookToLibrary();
-//console.log(myLibrary);
+function clearForm() {
+    let name = document.querySelector("#title")
+    name.value = "";
+    let author = document.querySelector("#author");
+    author.value = "";
+    let pages = document.querySelector("#pages");
+    pages.value = "";
+    let status = document.getElementsByName("status");
+    for (let i = 0; i < status.length; i++) {
+        status[i].checked = false;
+    }
+}
+
+displayBooks();
+
+newBtn.addEventListener("click", (e) => {
+    let name = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let pages = document.getElementById("pages").value;
+    let read = document.getElementsByName("status").value;
+
+    let newBook = new Book(name, author, pages, read);
+    myLibrary.push(newBook);
+    const newDiv = document.createElement("div");
+    let bookInfo = newBook.card();
+    newDiv.textContent = bookInfo;
+    const currentDiv = document.getElementById("main");
+    document.body.appendChild(newDiv);
+    e.preventDefault();
+    clearForm();
+});
+
 
